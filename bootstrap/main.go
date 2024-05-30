5e361704df9cd5042290df5e53d49435be8af100
Riccardo Piccoli <rpiccoli@redhat.com>
2024-05-30 15:35:49 +0200

    MGMT-17974: remove hack for providerID on baremetal platform (#8)
    Signed-off-by: Riccardo Piccoli <rpiccoli@redhat.com>

--- bootstrap/main.go
+++ bootstrap/main.go
@@ -156,13 +156,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Agent")
 		os.Exit(1)
 	}
-	if err = (&controller.AgentClusterInstallReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "AgentClusterInstall")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
