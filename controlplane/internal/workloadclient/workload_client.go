feee50f48334c236d16f15a8034ab729eabdffc0
CrystalChun <cchun@redhat.com>
2025-02-12 17:26:09 -0600

    MGMT-19876: Create workload cluster client package
    Workload cluster client gets the kube client
    for the workload cluster.

---
+++ controlplane/internal/workloadclient/workload_client.go
@@ -0,0 +1,31 @@
+package workloadclient
+
+import (
+	configv1 "github.com/openshift/api/config/v1"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/clientcmd"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func GetWorkloadClusterClient(kubeconfig []byte) (client.Client, error) {
+	clientConfig, err := clientcmd.NewClientConfigFromBytes(kubeconfig)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get clientconfig from kubeconfig data")
+	}
+
+	restConfig, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get restconfig for kube client")
+	}
+
+	schemes := runtime.NewScheme()
+	if err := configv1.Install(schemes); err != nil {
+		return nil, err
+	}
+	targetClient, err := client.New(restConfig, client.Options{Scheme: schemes})
+	if err != nil {
+		return nil, err
+	}
+	return targetClient, nil
+}
